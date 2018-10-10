4186691021e48b1596cad0447786f0800e1d9c9f
buttercup <a.stollberger@web.de>
2018-10-10 09:32:14 +0200

    added init Project in c++

---
+++ cmake-build-debug/CMakeFiles/MedienInfo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MedienInfo.dir/main.cpp.o"
+  "MedienInfo.pdb"
+  "MedienInfo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MedienInfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
