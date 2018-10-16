63e3cba081015217cd697107a4a3d184e126f4bf
Sebastian <sebb@sebb767.de>
2018-10-16 16:03:15 +0200

    Reading width works

--- PPMParser.h
+++ PPMParser.h
@@ -20,15 +20,41 @@ public:
     void parsePPM() {
         fstream FileBin("../output/test.ppm", ios::in|ios::binary);
         if (FileBin.is_open()) {
+            // header
             uint16_t header = 0;
             FileBin.read((char *) &header, sizeof(header));
             cout << header;
             if ('3P' != header) {
                 throw invalid_argument("Invalid Magic Number");
             }
+
+            // size
             uint8_t temp;
+
+            do {
+                FileBin.read((char *)&temp, sizeof(temp));
+                temp -= '0';
+            } while(temp >= 10);
+
+
+            int width = temp;
+
+            FileBin.read((char *)&temp, sizeof(temp));
+            temp -= '0';
+            
+            while (temp < 10) {
+                width *= 10;
+                width += temp;
+
+                FileBin.read((char *)&temp, sizeof(temp));
+                temp -= '0';
+            }
+
+            cout << "w: " << width;
         }
     }
+
+
 };
 
 #endif //MEDIENINFO_PPMPARSER_H
