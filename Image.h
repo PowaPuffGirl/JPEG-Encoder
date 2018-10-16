e846f3f6954efd17b657307b1530143e59ef7e57
Sebastian <sebb@sebb767.de>
2018-10-16 16:39:54 +0200

    basic ppm parsing

---
+++ Image.h
@@ -0,0 +1,43 @@
+//
+// Created by proj on 10/16/18.
+//
+
+#ifndef MEDIENINFO_IMAGE_H
+#define MEDIENINFO_IMAGE_H
+
+#include <vector>
+#include <stdexcept>
+
+struct RawImage {
+    const unsigned int width, height, colorDepth;
+    const float colorDepth_f;
+    std::vector<float> Y, Cb, Cr;
+
+    RawImage(unsigned int width, unsigned int height, unsigned int colorDepth)
+    : width(width), height(height), colorDepth(colorDepth), colorDepth_f(colorDepth), Y(), Cb(), Cr()
+    {
+        const auto vsize = width * height * sizeof(float);
+        Y.resize(vsize, 0);
+        Cb.resize(vsize, 0);
+        Cr.resize(vsize, 0);
+    }
+
+    void setValue(unsigned int offset, unsigned int red_i, unsigned int green_i, unsigned int blue_i) {
+        if(offset >= Y.size())
+            throw std::invalid_argument("Offset of pixel out of range!");
+
+        const float red = (float)red_i/colorDepth_f;
+        const float green = (float)green_i/colorDepth_f;
+        const float blue = (float)blue_i/colorDepth_f;
+
+        float y = 0.299f * red + 0.587f * green + 0.114f * blue;
+        float cb = -0.1687f * red + -0.3312f * green + 0.5f * blue + 0.5f;
+        float cr = 0.5f * red + -0.4186f * green + -0.0813f * blue + 0.5f;
+
+        Y.at(offset) = y;
+        Cb.at(offset) = cb;
+        Cr.at(offset) = cr;
+    }
+};
+
+#endif //MEDIENINFO_IMAGE_H
--- PPMParser.h
+++ PPMParser.h
@@ -6,6 +6,7 @@
 #include <stdexcept>
 
 #include "PixelTypes.h"
+#include "Image.h"
 
 
 using namespace std;
@@ -18,7 +19,7 @@ public:
     }
 
     void parsePPM() {
-        fstream FileBin("../output/test.ppm", ios::in|ios::binary);
+        fstream FileBin("../output/test.ppm", ios::in | ios::binary);
         if (FileBin.is_open()) {
 
             // read the first two bytes (header)
@@ -29,39 +30,64 @@ public:
             }
 
             // read the first three integeres (width, height, maxvalue)
-            int width = getNextInteger(FileBin);
-            int height = getNextInteger(FileBin);
-            int colordepth = getNextInteger(FileBin);
+            const unsigned int width = getNextInteger(FileBin);
+            const unsigned int height = getNextInteger(FileBin);
+            const unsigned int colordepth = getNextInteger(FileBin);
 
             cout << "w: " << width << " h:" << height << " mv:" << colordepth << "\n";
+
+            RawImage rawImage(width, height, colordepth);
+
+            unsigned int r, g, b, offset = 0;
+            try {
+
+                while (FileBin.good()) {
+                    r = getNextInteger(FileBin);
+                    g = getNextInteger(FileBin);
+                    b = getNextInteger(FileBin);
+
+                    rawImage.setValue(offset++, r, g, b);
+                }
+            }
+            catch (invalid_argument &ia) {
+                cout << "end of stream";
+            }
+        } else {
+            throw invalid_argument("Couldn't open file!");
         }
     }
 
 private:
-    int getNextInteger(fstream& stream) {
+
+    unsigned int getNextInteger(fstream &stream) {
         unsigned char temp;
 
         // read single bytes until a number is found
         do {
-            stream.read((char *)&temp, sizeof(temp));
-            temp -= '0';
-        } while(temp >= 10);
+            readNum(stream, &temp);
+        } while (temp >= 10);
 
         // when the whitespace loop exits temp contains the first ascii number
-        int result = temp;
-        stream.read((char *)&temp, sizeof(temp));
-        temp -= '0';
+        unsigned int result = temp;
+        readNum(stream, &temp);
 
         // loop until an non-number ascii value is found
         while (temp < 10) {
             result *= 10;
             result += temp;
 
-            stream.read((char *)&temp, sizeof(temp));
-            temp -= '0';
+            readNum(stream, &temp);
         }
 
         return result;
+    }
+
+    void readNum(fstream &stream, unsigned char *temp) {
+        if (stream.bad())
+            throw invalid_argument("Got invalid stream as parameter!");
+
+        stream.read((char *) &temp, sizeof(temp));
+        temp -= '0';
     }
 
 
