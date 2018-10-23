d8bc0a7091eb86d35543cd954a154f71f601465b
Sebastian <sebb@sebb767.de>
2018-10-23 19:03:08 +0200

    Added threading

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include "PPMParser.h"
 
 const unsigned int stepSize = 16;
@@ -7,11 +8,24 @@ int main() {
     PPMParser test(stepSize, stepSize);
     RawImage temp = test.parsePPM();
 
-    temp.exportPPMSubsampled420simple("420simple");
-    temp.exportPPMSubsampled420average("420average");
-    temp.exportPPMSubsampled411("411");
-    temp.exportPPMSubsampled422("422");
+    std::thread t1([&temp](){
+        temp.exportPPMSubsampled420simple("420simple");
+    });
+    std::thread t2([&temp](){
+        temp.exportPPMSubsampled420average("420average");
+    });
+    std::thread t3([&temp](){
+        temp.exportPPMSubsampled411("411");
+    });
+    std::thread t4([&temp](){
+        temp.exportPPMSubsampled422("422");
+    });
     temp.exportPPMSubsampled444("444");
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
 
     return 0;
 }
