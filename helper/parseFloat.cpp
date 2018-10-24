6598eeaaabc24218cdeb2a1465b8303d3a566aff
Sebastian <sebb@sebb767.de>
2018-10-24 08:19:02 +0200

    Removed old unused code

--- helper/parseFloat.cpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include "parseFloat.h"
-
-void parseFloatSimple(char* str, int len, float* result) {
-    std::string dt(str, len);
-    *result = (float)atoi(dt.c_str());
-}
--- helper/parseFloat.h
+++
@@ -1,7 +0,0 @@
-#ifndef MEDIENINFO_PARSEFLOAT_H
-#define MEDIENINFO_PARSEFLOAT_H
-
-#define PARSEFLOAT_IMPL parseFloatSimple
-#define parseFloat(str, strlen, result) PARSEFLOAT_IMPL(str, strlen, result)
-
-#endif //MEDIENINFO_PARSEFLOAT_H
