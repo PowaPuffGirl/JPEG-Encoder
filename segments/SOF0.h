a2cbe3aa45c93f0f51546be9a1ccc625ebb8f2e7
buttercup <a.stollberger@web.de>
2018-10-29 13:27:18 +0100

    translated comments in SoF0

--- segments/SOF0.h
+++ segments/SOF0.h
@@ -4,25 +4,25 @@
 #include <cstdint>
 
 struct SOF0 {
-    const uint16_t marker = 0xc0ff; //marker
-    const uint16_t len = 8+componentAmount*3; //segment länge
-    const uint8_t BitsPerSample = 8; //Genauigkeit der Daten
-    uint16_t imageWidth;    //image Breite
-    uint16_t imageHeight;   //image Höhe
-    const uint8_t componentAmount = 3; //Anzahl der verwendete Componenten 1 = nur Y, 3 = alle Kanäle
+    const uint16_t marker = 0xc0ff;
+    const uint16_t len = 8+componentAmount*3; // segment length without marker
+    const uint8_t BitsPerSample = 8; // prescicion of the data
+    uint16_t imageHeight;   // image height
+    uint16_t imageWidth;    // image width
+    const uint8_t componentAmount = 3; // amount of used channels 1 = only Y, 3 = all channels
 
-    const uint8_t YcompNumber = 1;  // Komponenten Nummer 1 = Y, 2 = Cb, 3 = Cr
-    const uint8_t YcompOversampling = 0x22;  //0x22 = keine unterabtastung, 0x11 = mit unterabtastung
-    const uint8_t YcompTableNumber = 5; //verwendete quantisierungstabelle für unterabtastung
+    const uint8_t YcompNumber = 1;  // Component Number 1 = Y, 2 = Cb, 3 = Cr
+    const uint8_t YcompOversampling = 0x22;  //0x22 = no subsampling, 0x11 = with subsampling
+    const uint8_t YcompTableNumber = 5; // used subsampling table
 
-    const uint8_t CBcompNumber = 2;  // Komponenten Nummer 1 = Y, 2 = Cb, 3 = Cr
-    const uint8_t CBcompOversampling = 0x22;  //0x22 = keine unterabtastung, 0x11 = mit unterabtastung
-    const uint8_t CBcompTableNumber = 5; //verwendete quantisierungstabelle für unterabtastung
+    const uint8_t CBcompNumber = 2;  // Component Number 1 = Y, 2 = Cb, 3 = Cr
+    const uint8_t CBcompOversampling = 0x22;  // 0x22 = no subsampling, 0x11 = with subsampling
+    const uint8_t CBcompTableNumber = 5; // used subsampling table
 
 
-    const uint8_t CRcompNumber = 3;  // Komponenten Nummer 1 = Y, 2 = Cb, 3 = Cr
-    const uint8_t CRcompOversampling = 0x22;  //0x22 = keine unterabtastung, 0x11 = mit unterabtastung
-    const uint8_t CRcompTableNumber = 5; //verwendete quantisierungstabelle für unterabtastung
+    const uint8_t CRcompNumber = 3;  // Component Number 1 = Y, 2 = Cb, 3 = Cr
+    const uint8_t CRcompOversampling = 0x22;  // 0x22 = no subsampling, 0x11 = with subsampling
+    const uint8_t CRcompTableNumber = 5; //used subsampling table
 
 
 
