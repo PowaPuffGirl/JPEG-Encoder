17b48b2a8f606abf28516f27f0bbd01427e40499
Sebastian <sebb@sebb767.de>
2018-10-30 16:02:50 +0100

    Fixed byte order and offsets in SOF0 segment

--- segments/SOF0.h
+++ segments/SOF0.h
@@ -6,8 +6,8 @@
 
 // this struct is fixed for 3 channels
 struct SOF0 {
-    const uint16_t marker = 0xFFC0;
-    const uint16_t len = 17; // segment length without marker = 8 + component_amount * 3
+    const uint16_t marker = convert_u16(0xFFC0);
+    const uint16_t len = convert_u16(17); // segment length without marker = 8 + component_amount * 3
     const uint8_t BitsPerSample = 8; // prescicion of the data
     uint16_t imageHeight;   // image height
     uint16_t imageWidth;    // image width
@@ -15,15 +15,15 @@ struct SOF0 {
 
     const uint8_t YcompNumber = 1;  // Component Number 1 = Y, 2 = Cb, 3 = Cr
     const uint8_t YcompOversampling = 0x22;  //0x22 = no subsampling, 0x11 = with subsampling
-    const uint8_t YcompTableNumber = 5; // used subsampling table
+    const uint8_t YcompTableNumber = 0; // used subsampling table
 
     const uint8_t CBcompNumber = 2;  // Component Number 1 = Y, 2 = Cb, 3 = Cr
     const uint8_t CBcompOversampling = 0x22;  // 0x22 = no subsampling, 0x11 = with subsampling
-    const uint8_t CBcompTableNumber = 5; // used subsampling table
+    const uint8_t CBcompTableNumber = 0; // used subsampling table
 
     const uint8_t CRcompNumber = 3;  // Component Number 1 = Y, 2 = Cb, 3 = Cr
     const uint8_t CRcompOversampling = 0x22;  // 0x22 = no subsampling, 0x11 = with subsampling
-    const uint8_t CRcompTableNumber = 5; //used subsampling table
+    const uint8_t CRcompTableNumber = 0; //used subsampling table
 
     SOF0(const int height, const int width) {
         imageHeight = convert_u16(static_cast<uint16_t>(height));
