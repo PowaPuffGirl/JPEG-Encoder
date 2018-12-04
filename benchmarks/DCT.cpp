9c69e87d3abbb5a7d4b13cd1eb0d7364a9ce5db6
Sebastian <sebb@sebb767.de>
2018-12-04 17:24:10 +0100

    Fixed output of DirectCosinusTransform

--- benchmarks/DCT.cpp
+++ benchmarks/DCT.cpp
@@ -4,21 +4,27 @@
 #include "../EncodingProcessor.h"
 #include "../dct/DirectCosinusTransform.h"
 //#include "../dct/SeparatedCosinusTransformGlm.h"
+#include "../dct/AraiCosinusTransform.h"
 
 template<typename Transform, typename T = float>
 static void TestConversionDeinzer(benchmark::State& state) {
-    auto sampleBuffer = generateDeinzerBuffer<T>();
-//    auto sampleBuffer = generateTestBuffer<T>(256, 256);
+//    auto sampleBuffer = generateDeinzerBuffer<T>();
+    auto sampleBuffer = generateTestBuffer<T>(256, 256);
     SampledWriter<T> outBuffer(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
+    SampledWriter<T> outBuffer2(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
 
     EncodingProcessor<T> encProc;
     Transform transform;
 
     for (auto _ : state) {
         encProc.processChannel(sampleBuffer, transform, outBuffer);
+        AraiCosinusTransform<float> act;
+        encProc.processChannel(sampleBuffer, act, outBuffer2);
+        int i = 0;
     }
 }
 
 
 BENCHMARK_TEMPLATE(TestConversionDeinzer, DirectCosinusTransform<float>);
 //BENCHMARK_TEMPLATE(TestConversionDeinzer, SeparatedCosinusTransform<float>);
+BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiCosinusTransform<float>);
--- dct/DirectCosinusTransform.h
+++ dct/DirectCosinusTransform.h
@@ -16,16 +16,17 @@ private:
     }
 
 public:
-    void transformBlock(std::function<const T&(uint, uint)> get, std::function<T&(uint, uint)> set) const {
+    void transformBlock(const std::function<const T&(uint, uint)>& get, const std::function<T&(uint, uint)>& set) const {
         for (unsigned int i = 0; i < blocksize; i++) {
             for (unsigned int j = 0; j < blocksize; j++) {
                double sum = 0;
                 for (unsigned int x = 0; x < blocksize; x++) {
                     for (unsigned int y = 0; y < blocksize; y++) {
-                        sum += get(x, y)*cos(((2*x+1)*i*M_PI)/2*blocksize) * cos(((2*y +1)*j*M_PI)/2*blocksize);
+                        auto z = get(x, y);
+                        sum += get(x, y)*cos(((2.*x+1)*i*M_PI)/2.*blocksize) * cos(((2.*y +1)*j*M_PI)/2.*blocksize);
                      }
                 }
-                set(i,j) = 2/blocksize * C(i)*C(j)*sum;
+                set(i,j) = 2./blocksize * C(i)*C(j)*sum;
             }
         }
     }
