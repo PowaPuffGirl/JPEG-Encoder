150598f3f479582030ba5e458c63776700d0801c
Sebastian <sebb@sebb767.de>
2018-12-04 17:46:56 +0100

    Tried own matrix implementation

--- dct/SeparatedCosinusTransformGlm.h
+++ dct/SeparatedCosinusTransformGlm.h
@@ -3,23 +3,25 @@
 
 
 #include "AbstractCosinusTransform.h"
-#include <glm/matrix.hpp>
-#include <glm/mat4x4.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
 
 template<typename T, unsigned int blocksize = 8>
-class SeparatedCosinusTransform : public AbstractCosinusTransform<T, blocksize> {
+class SeparatedCosinusTransform {
 private:
-    using mat8x8 = glm::mat<blocksize, blocksize, T, glm::defaultp>;
+    using mat8x8 = std::array<T, blocksize*blocksize>;
+    const mat8x8 A = generateA();
+    const mat8x8 AT = generateAT();
 
-    double C(int n) {
+    constexpr double C(int n) const {
         if(n == 0) {
-            return 1./sqrt(2);
+            return M_SQRT1_2;
         }
         return 1.;
     }
 
-    mat8x8 generateA() {
+    constexpr mat8x8 generateA() const {
         mat8x8 A;
         const T N = blocksize;
         const double prefix = sqrt(2./N);
@@ -27,39 +29,63 @@ private:
 
         for (uint k = 0; k < blocksize; ++k) {
             for (uint n = 0; n < blocksize; ++n) {
-                A(k, n) = C(k) * prefix * cos(((n << 1) + 1) * k * suffix);
+                A[(k << 3) + n] = C(k) * prefix * cos(((n << 1) + 1) * k * suffix);
             }
         }
 
         return A;
     }
 
-    mat8x8 generateAT() {
-        return glm::transpose(generateA());
-    }
+    mat8x8 generateAT() const {
+        mat8x8 B;
+        mat8x8 A = generateA();
 
-    void generateMatFromChannel(const std::function<T&(uint, uint)>& get, mat8x8& matrix) {
-        for (uint y = 0; y < blocksize; ++y) {
-            for (uint x = 0; x < blocksize; ++x) {
-                matrix(x, y) = get(x, y);
+        for (int i = 0; i < blocksize; ++i) {
+            for (int j = 0; j < blocksize; ++j) {
+                B[(i << 3) + j] = A[(j << 3) + i];
             }
         }
     }
 
-    void writeMat(const std::function<T&(uint, uint)>& set, mat8x8& matrix) {
+    void generateMatFromChannel(const std::function<const T&(uint, uint)>& get, mat8x8& matrix) const {
         for (uint y = 0; y < blocksize; ++y) {
             for (uint x = 0; x < blocksize; ++x) {
-                set(x, y) = matrix(x, y);
+                matrix[(x << 3) + y] = get(x, y);
+            }
+        }
+    }
+
+    void writeMat(const std::function<T&(uint, uint)>& set, mat8x8& matrix) const {
+        for (uint y = 0; y < blocksize; ++y) {
+            for (uint x = 0; x < blocksize; ++x) {
+                set(x, y) = matrix[(x << 3) + y];
             }
         }
     }
 
 public:
-    void transformBlock(const std::function<T&(uint, uint)>& get, const std::function<T&(uint, uint)>& set) {
+    void transformBlock(const std::function<const T&(uint, uint)>& get, const std::function<T&(uint, uint)>& set) const {
         mat8x8 X;
         generateMatFromChannel(get, X);
-        mat8x8 Y = generateA() * X * generateAT();
-        writeMat(set, Y);
+        mat8x8 Y = {0}, Y2 = {0}; // generateA() * X * generateAT();
+
+        for (int i = 0; i < blocksize; ++i) { // col
+            for (int j = 0; j < blocksize; ++j) { // row
+                for (int r = 0; r < blocksize; ++r) { // indey
+                    Y[(i << 3) + j] += A[(i << 3) + r] + X[(r << 3) + j];
+                }
+            }
+        }
+
+        for (int i = 0; i < blocksize; ++i) { // col
+            for (int j = 0; j < blocksize; ++j) { // row
+                for (int r = 0; r < blocksize; ++r) { // indey
+                    Y2[(i << 3) + j] += Y[(i << 3) + r] + AT[(r << 3) + j];
+                }
+            }
+        }
+
+        writeMat(set, Y2);
     }
 };
 
