4cb59dee575fe8bbc91014c676190f0891942252
Sebastian <sebb@sebb767.de>
2018-12-12 23:14:46 +0100

    Fixed uint error in Windows, removed useless base class

--- dct/AbstractCosinusTransform.h
+++ dct/AbstractCosinusTransform.h
@@ -5,14 +5,6 @@
 #include <functional>
 #include "../Image.h"
 
-template<typename T, unsigned int blocksize = 8>
-class AbstractCosinusTransform {
-public:
-    using uint = unsigned int;
-
-    AbstractCosinusTransform() = default;
-    virtual void transformBlock(std::function<T&(uint, uint)> get, std::function<T&(uint, uint)> set) = 0;
-};
-
+using uint = unsigned int;
 
 #endif //MEDIENINFO_DISCRETECOSINUSTRANSFORM_H
