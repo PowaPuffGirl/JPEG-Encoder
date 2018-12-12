0a94043781c9fd09d2a3e675d9a02f552d3bb00a
buttercup <a.stollberger@web.de>
2018-12-12 15:11:20 +0100

    added book implementation of arai

--- benchmarks/DCT.cpp
+++ benchmarks/DCT.cpp
@@ -7,6 +7,7 @@
 #include "../dct/SeparatedCosinusTransformSimd.h"
 #include "../dct/AraiCosinusTransform.h"
 #include "../dct/AraiSimd.h"
+#include "../dct/AraiBookCosinusTransform.h"
 
 template<typename Transform, typename T = float>
 static void TestConversionDeinzer(benchmark::State& state) {
@@ -32,6 +33,7 @@ static void TestConversionAll(benchmark::State& state) {
     SampledWriter<T> outBufferArai(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
     SampledWriter<T> outBufferAraiS(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
     SampledWriter<T> outBufferSCTS(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
+    SampledWriter<T> outBufferAraiBook(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
 
     EncodingProcessor<T> encProc;
     AraiCosinusTransform<T> act;
@@ -39,6 +41,7 @@ static void TestConversionAll(benchmark::State& state) {
     DirectCosinusTransform<T> dct;
     SeparatedCosinusTransform<T> sct;
     SeparatedCosinusTransformSimd<T> scts;
+    AraiBookCosinusTransform<T> araiBook;
 
     for (auto _ : state) {
         encProc.processChannel(sampleBuffer, act, outBufferArai);
@@ -46,6 +49,7 @@ static void TestConversionAll(benchmark::State& state) {
         encProc.processChannel(sampleBuffer, sct, outBufferSCT);
         encProc.processChannel(sampleBuffer, scts, outBufferSCTS);
         encProc.processChannel(sampleBuffer, araiS, outBufferAraiS);
+        encProc.processChannel(sampleBuffer, araiBook, outBufferAraiBook);
     }
 
     state.counters["Arai <-> SCT"] = outBufferArai.errorTo(outBufferSCT);
@@ -53,6 +57,7 @@ static void TestConversionAll(benchmark::State& state) {
     state.counters["DCT <-> SCT"] = outBufferDCT.errorTo(outBufferSCT);
     state.counters["SCTs <-> SCT"] = outBufferSCTS.errorTo(outBufferSCT);
     state.counters["AraiS <-> SCT"] = outBufferAraiS.errorTo(outBufferSCT);
+    state.counters["AraiBook <-> DCT"] = outBufferAraiBook.errorTo(outBufferDCT);
 }
 
 BENCHMARK(TestConversionAll);
@@ -61,3 +66,4 @@ BENCHMARK_TEMPLATE(TestConversionDeinzer, SeparatedCosinusTransform<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiCosinusTransform<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, SeparatedCosinusTransformSimd<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiSimd<float>);
+BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiBookCosinusTransform<float>);
