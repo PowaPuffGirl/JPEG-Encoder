b49c90f69bb59a2a97b700d1d45abddbbc6b64de
Sebastian <sebb@sebb767.de>
2018-12-18 23:52:26 +0100

    Added 8x8 Block tests

--- benchmarks/DCT.cpp
+++ benchmarks/DCT.cpp
@@ -24,6 +24,20 @@ static void TestConversionDeinzer(benchmark::State& state) {
     }
 }
 
+template<typename Transform, typename T = float>
+static void TestConversionSmall(benchmark::State& state) {
+    auto sampleBuffer = generateTestBuffer<T>(8, 8);
+    SampledWriter<T> outBuffer(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
+    SampledWriter<T> outBuffer2(sampleBuffer.widthPadded, sampleBuffer.heightPadded);
+
+    EncodingProcessor<T> encProc;
+    Transform transform;
+
+    for (auto _ : state) {
+        encProc.processChannel(sampleBuffer, transform, outBuffer);
+    }
+}
+
 template<typename T = float>
 static void TestConversionAll(benchmark::State& state) {
     auto sampleBuffer = generateDeinzerBuffer<T>();
@@ -62,11 +76,21 @@ static void TestConversionAll(benchmark::State& state) {
 }
 
 //BENCHMARK(TestConversionAll);
+
 BENCHMARK_TEMPLATE(TestConversionDeinzer, DirectCosinusTransform<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, SeparatedCosinusTransform<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiCosinusTransform<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, SeparatedCosinusTransformSimd<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiSimd<float>);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiSimdSimple<float>);
-BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiSimdSimple<short>, short);
 BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiSimdSimple<int32_t>, int32_t);
+BENCHMARK_TEMPLATE(TestConversionDeinzer, AraiSimdSimple<short>, short);
+
+BENCHMARK_TEMPLATE(TestConversionSmall, DirectCosinusTransform<float>);
+BENCHMARK_TEMPLATE(TestConversionSmall, SeparatedCosinusTransform<float>);
+BENCHMARK_TEMPLATE(TestConversionSmall, AraiCosinusTransform<float>);
+BENCHMARK_TEMPLATE(TestConversionSmall, SeparatedCosinusTransformSimd<float>);
+BENCHMARK_TEMPLATE(TestConversionSmall, AraiSimd<float>);
+BENCHMARK_TEMPLATE(TestConversionSmall, AraiSimdSimple<float>);
+BENCHMARK_TEMPLATE(TestConversionSmall, AraiSimdSimple<int32_t>, int32_t);
+BENCHMARK_TEMPLATE(TestConversionSmall, AraiSimdSimple<short>, short);
