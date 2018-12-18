755848ac098a19baebbd747a18c68d4c0361a031
Sebastian <sebb@sebb767.de>
2018-12-18 14:58:22 +0100

    added arai access in AraiSimdSimple

--- dct/AraiSimdSimple.h
+++ dct/AraiSimdSimple.h
@@ -11,6 +11,28 @@ class AraiSimdSimple {
     using vec8 = Vc::fixed_size_simd<T, blocksize>;
     using rowBlock = std::array<vec8, blocksize>;
 
+    std::array<T, 6> a = {
+            0,
+            a1,
+            a2,
+            a3,
+            a4,
+            a5
+    };
+
+
+    std::array<T, 8> s = {
+            s0,
+            s1,
+            s2,
+            s3,
+            s4,
+            s5,
+            s6,
+            s7
+    };
+
+
 public:
     void transformBlock(const std::function<const T &(uint, uint)>& get, const std::function<T &(uint, uint)>& set) const {
         vec8 y0,y1,y2,y3,y4,y5,y6,y7 = 0;
@@ -36,34 +58,34 @@ public:
         ty6 = y6 + y7;
 
         y1 = ty0 - ty1;
-        ty0 = (ty0 + ty1) * s0;
-        ty2 = (ty2 + ty3) * a1;
+        ty0 = (ty0 + ty1) * s[0];
+        ty2 = (ty2 + ty3) * a[1];
         y4 = ty4;
 
-        ty4 = y4*a2 - ((ty6-y4)*a5);
-        ty6 = ty6*a4 - ((ty6+y4)*a5);
+        ty4 = y4*a[2] - ((ty6-y4)*a[5]);
+        ty6 = ty6*a[4] - ((ty6+y4)*a[5]);
 
         y2 = ty2 + ty3;
-        ty3 = (ty3 - ty2) * s6;
-        y5 = (ty5 * a3) + y7;
+        ty3 = (ty3 - ty2) * s[6];
+        y5 = (ty5 * a[3]) + y7;
         y7 = y7 - ty5;
 
-        ty1 = y1 * s4;
-        ty2 = y2 * s2;
-        ty7 = (y7 - ty4) * s3;
-        ty4 = (ty4 + y7) * s5;
-        ty5 = (y5 + ty6) * s1;
-        ty6 = (y5 - ty6) * s7;
+        ty1 = y1 * s[4];
+        ty2 = y2 * s[2];
+        ty7 = (y7 - ty4) * s[3];
+        ty4 = (ty4 + y7) * s[5];
+        ty5 = (y5 + ty6) * s[1];
+        ty6 = (y5 - ty6) * s[7];
 
         std::array<vec8*, 8> rb = {
-            &ty0,
-            &ty5,
-            &ty2,
-            &ty7,
-            &ty1,
-            &ty4,
-            &ty3,
-            &ty6
+                &ty0,
+                &ty5,
+                &ty2,
+                &ty7,
+                &ty1,
+                &ty4,
+                &ty3,
+                &ty6
         };
         for(uint i = 0; i < 8; i++) {
             const auto& c = *rb[i];
@@ -86,24 +108,24 @@ public:
         ty6 = y6 + y7;
 
         y1 = ty0 - ty1;
-        ty0 = (ty0 + ty1) * s0;
-        ty2 = (ty2 + ty3) * a1;
+        ty0 = (ty0 + ty1) * s[0];
+        ty2 = (ty2 + ty3) * a[1];
         y4 = ty4;
 
-        ty4 = y4*a2 - ((ty6-y4)*a5);
-        ty6 = ty6*a4 - ((ty6+y4)*a5);
+        ty4 = y4*a[2] - ((ty6-y4)*a[5]);
+        ty6 = ty6*a[4] - ((ty6+y4)*a[5]);
 
         y2 = ty2 + ty3;
-        ty3 = (ty3 - ty2) * s6;
-        y5 = (ty5 * a3) + y7;
+        ty3 = (ty3 - ty2) * s[6];
+        y5 = (ty5 * a[3]) + y7;
         y7 = y7 - ty5;
 
-        ty1 = y1 * s4;
-        ty2 = y2 * s2;
-        ty7 = (y7 - ty4) * s3;
-        ty4 = (ty4 + y7) * s5;
-        ty5 = (y5 + ty6) * s1;
-        ty6 = (y5 - ty6) * s7;
+        ty1 = y1 * s[4];
+        ty2 = y2 * s[2];
+        ty7 = (y7 - ty4) * s[3];
+        ty4 = (ty4 + y7) * s[5];
+        ty5 = (y5 + ty6) * s[1];
+        ty6 = (y5 - ty6) * s[7];
 
         for(uint i = 0; i < 8; i++) {
             set(0,i) = ty0[i];
