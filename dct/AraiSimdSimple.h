9c4ef7e7d1835bc505767073c154f42af21bf4ec
Sebastian <sebb@sebb767.de>
2018-12-18 15:06:02 +0100

    Further optimized AraiSimdSimple

--- dct/AraiSimdSimple.h
+++ dct/AraiSimdSimple.h
@@ -8,6 +8,7 @@
 
 template<typename T, unsigned int blocksize = 8>
 class AraiSimdSimple {
+private:
     using vec8 = Vc::fixed_size_simd<T, blocksize>;
     using rowBlock = std::array<vec8, blocksize>;
 
@@ -32,22 +33,42 @@ class AraiSimdSimple {
             s7
     };
 
+    vec8 y0,y1,y2,y3,y4,y5,y6,y7 = 0;
+    vec8 ty0, ty1, ty2, ty3, ty4, ty5, ty6, ty7 = 0;
+    std::array<vec8*, 8> rb = {
+            &ty0,
+            &ty5,
+            &ty2,
+            &ty7,
+            &ty1,
+            &ty4,
+            &ty3,
+            &ty6
+    };
 
 public:
-    void transformBlock(const std::function<const T &(uint, uint)>& get, const std::function<T &(uint, uint)>& set) const {
-        vec8 y0,y1,y2,y3,y4,y5,y6,y7 = 0;
-        vec8 ty0, ty1, ty2, ty3, ty4, ty5, ty6, ty7 = 0;
+    void transformBlock(const std::function<const T &(uint, uint)>& get, const std::function<T &(uint, uint)>& set) {
         for(uint i = 0; i < 8; i++) {
-            y0[i] = get(i,0) + get(i,7);
-            y1[i] = get(i,1) + get(i,6);
-            y2[i] = get(i,2) + get(i,5);
-            y3[i] = get(i,3) + get(i,4);
-            y4[i] = get(i,3) - get(i,4);
-            y5[i] = get(i,2) - get(i,5);
-            y6[i] = get(i,1) - get(i,6);
-            y7[i] = get(i,0) - get(i,7);
-
+            y0[i] = get(i,0);
+            y1[i] = get(i,1);
+            y2[i] = get(i,2);
+            y3[i] = get(i,3);
+            ty4[i] = get(i,4);
+            ty5[i] = get(i,5);
+            ty6[i] = get(i,6);
+            ty7[i] = get(i,7);
         }
+
+        y4 = y3 - ty4;
+        y5 = y2 - ty5;
+        y6 = y1 - ty6;
+        y7 = y0 - ty7;
+        y0 += ty7;
+        y1 += ty6;
+        y2 += ty5;
+        y3 += ty4;
+
+
 
         ty0 = y0 + y3;
         ty1 = y1 + y2;
@@ -77,16 +98,6 @@ public:
         ty5 = (y5 + ty6) * s[1];
         ty6 = (y5 - ty6) * s[7];
 
-        std::array<vec8*, 8> rb = {
-                &ty0,
-                &ty5,
-                &ty2,
-                &ty7,
-                &ty1,
-                &ty4,
-                &ty3,
-                &ty6
-        };
         for(uint i = 0; i < 8; i++) {
             const auto& c = *rb[i];
             y0[i] = c[0] + c[7];
