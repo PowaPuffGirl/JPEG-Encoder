e5607d5491da038993bbb14ef76cdd5f8aceb6bd
Sebastian <sebb@sebb767.de>
2019-01-07 17:39:46 +0100

    Added converter from SampledWriter to OffsetSampledWriter

--- SampledWriter.h
+++ SampledWriter.h
@@ -8,54 +8,6 @@
 #include <iostream>
 #include "quantisation/quantisationTables.h"
 
-template<typename T>
-class SampledWriter {
-private:
-    using uint = unsigned int;
-
-    std::vector<T> output;
-    const uint xsize, ysize;
-    const uint blocksize = 8;
-
-public:
-    SampledWriter(const uint xsize, const uint ysize) : xsize(xsize), ysize(ysize) {
-        output.resize(xsize * ysize, 0);
-    }
-
-    std::function<T&(uint, uint)> getBlockSetter(uint blockx, uint blocky) {
-        assert(blocksize == 8); // for << 3
-
-        auto vector = &output;
-        const auto pos = blocky * xsize + blockx;
-        return [vector, pos](uint x, uint y) -> T& {
-            return (*vector).at(pos + (y << 3) + x);
-        };
-    }
-
-    bool compareWith(SampledWriter<T> other) {
-        assert(other.output.size() == output.size());
-        for (int i = 0; i < output.size(); i++) {
-            if (output[i] != other.output[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    double errorTo(SampledWriter<T> other) {
-        assert(other.output.size() == output.size());
-        double errsum = 0;
-        for (int i = 0; i < output.size(); i++) {
-            if (output[i] != other.output[i]) {
-                errsum += abs(output[i] - other.output[i]);
-                //auto err = abs(output[i] - other.output[i]);
-                //errsum += err*err;
-            }
-        }
-        //return sqrt(errsum / output.size());
-        return errsum / output.size();
-    }
-};
 
 class ZikZakLookupTable {
 private:
@@ -153,5 +105,68 @@ public:
     }
 };
 
+
+template<typename T>
+class SampledWriter {
+private:
+    using uint = unsigned int;
+
+    std::vector<T> output;
+    const uint xsize, ysize;
+    const uint blocksize = 8;
+
+public:
+    SampledWriter(const uint xsize, const uint ysize) : xsize(xsize), ysize(ysize) {
+        output.resize(xsize * ysize, 0);
+    }
+
+    std::function<T&(uint, uint)> getBlockSetter(uint blockx, uint blocky) {
+        assert(blocksize == 8); // for << 3
+
+        auto vector = &output;
+        const auto pos = blocky * xsize + blockx;
+        return [vector, pos](uint x, uint y) -> T& {
+            return (*vector).at(pos + (y << 3) + x);
+        };
+    }
+
+    OffsetSampledWriter<T> toOffsetSampledWriter(const QuantisationTable& qt) {
+        OffsetSampledWriter<T> outputw((xsize * ysize) / (blocksize * blocksize), qt);
+        const auto widthInBlocks = xsize / blocksize;
+
+        int offset = 0;
+        for (uint x = 0; x < xsize; ++x) {
+            for (uint y = 0; y < ysize; ++y) {
+                outputw.set(output.at(offset++), ((y / 8) * widthInBlocks) + (x / 8), x % 8, y % 8);
+            }
+        }
+
+        return outputw;
+    }
+
+    bool compareWith(SampledWriter<T> other) {
+        assert(other.output.size() == output.size());
+        for (int i = 0; i < output.size(); i++) {
+            if (output[i] != other.output[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    double errorTo(SampledWriter<T> other) {
+        assert(other.output.size() == output.size());
+        double errsum = 0;
+        for (int i = 0; i < output.size(); i++) {
+            if (output[i] != other.output[i]) {
+                errsum += abs(output[i] - other.output[i]);
+                //auto err = abs(output[i] - other.output[i]);
+                //errsum += err*err;
+            }
+        }
+        //return sqrt(errsum / output.size());
+        return errsum / output.size();
+    }
+};
 
 #endif //MEDIENINFO_SAMPLEDWRITER_H
