6dd50c82dba584a11d52190cb604fb6bd880b4ed
Sebastian <sebb@sebb767.de>
2019-01-16 19:11:15 +0100

    Added bitstream deinzer

--- BitStream.h
+++ BitStream.h
@@ -13,7 +13,7 @@
 
 #define _write_segment_ref(bitstream, segment) bitstream.writeBytes(&segment, sizeof(segment))
 
-class BitStream {
+class BitStreamSeb {
 private:
     uint8_t* streamStart;
     const std::string fileName;
@@ -25,7 +25,7 @@ private:
     const int height;
 
 public:
-    BitStream(std::string fileName, const unsigned int width, const unsigned int height) :
+    BitStreamSeb(std::string fileName, const unsigned int width, const unsigned int height) :
         width(width), height(height), fileName(std::move(fileName))
     {
         size = 1024 + width * height * 24; // byte approximation for memory usage
@@ -36,7 +36,7 @@ public:
             throw std::bad_alloc();
     }
 
-    ~BitStream() {
+    ~BitStreamSeb() {
         free(streamStart);
     }
 
@@ -189,5 +189,130 @@ private:
     };
 
 };
+
+
+
+class BitStreamDeinzer {
+private:
+    uint8_t* streamStart;
+    const std::string fileName;
+    uint64_t position = 0; // byte position
+    uint8_t position_bit = 0; // inner bit position
+    uint64_t size;
+    uint64_t buffer = 0;
+    const int width;
+    const int height;
+
+public:
+    BitStreamDeinzer(std::string fileName, const unsigned int width, const unsigned int height) :
+        width(width), height(height), fileName(std::move(fileName))
+    {
+        size = 1024 + width * height * 24; // byte approximation for memory usage
+        streamStart = static_cast<uint8_t *>(malloc(size));
+
+        if(streamStart == nullptr)
+            throw std::bad_alloc();
+    }
+
+    ~BitStreamDeinzer() {
+        free(streamStart);
+    }
+
+    void writeBytes(const void* bytes, const size_t len) {
+        assert((position + static_cast<uint64_t>(len)) < size);
+        assert(position_bit == 0);
+
+        std::memcpy(streamStart + position, bytes, len);
+        position += static_cast<uint64_t>(len);
+    }
+
+    inline void writeByteAligned(const uint8_t b) {
+        assert((position + 1)  < size);
+        writeByteAlignedUnsafe(b);
+    }
+
+
+    void appendU16(uint16_t data, const uint8_t amount) {
+        assert(amount > 0);
+        assert(amount <= 16);
+        //assert((data & ~ones(amount)) == 0); // check all unimportant bytes are zero
+        assert(size > ((position << 3) + position_bit + amount) / 8);
+        data &= ~ones(amount);
+
+        buffer <<= amount;
+        buffer |= data;
+        position_bit += amount;
+
+        while(position_bit >= 8) {
+            uint64_t tmp = (buffer >> (position_bit - 8));
+            uint8_t tbuffer = static_cast<uint8_t>(tmp);
+            assert((tmp & ones(8)) == tbuffer); // check all unimportant bytes are zero
+            writeByteAlignedUnsafe(tbuffer);
+            position_bit -= 8;
+        }
+
+    }
+
+    /**
+     * Write up to 8 bits to the stream
+     */
+    void appendBit(const uint8_t data, const uint8_t amount) {
+
+        appendU16(data, amount);
+    }
+
+    /**
+     * Fill a partially filled byte with ones.
+     */
+    void fillByte() {
+        if(position_bit == 0)
+            return;
+
+        appendBit(static_cast<const uint8_t>(ones(8 - position_bit)), 8 - position_bit);
+        assert(position_bit == 0);
+    }
+
+    /**
+     * Save the stream to disk
+     */
+    void writeOut() {
+        fillByte();
+        auto file = std::fstream(fileName, std::ios::out | std::ios::binary);
+        file.write((char*)streamStart, position);
+        file.close();
+    }
+
+private:
+    inline void writeByteAlignedUnsafe(const uint8_t b) {
+        assert((position + 1)  < size);
+
+        *(streamStart + position) = b;
+        ++position;
+    }
+
+
+    /**
+     * Return an uint64_t with the rightmost $amount bytes set to one
+     */
+    static constexpr uint64_t ones(const int amount) {
+        assert(amount < 63 && amount >= 0);
+        return (static_cast<uint64_t>(1) << amount) - 1;
+    }
+
+    static constexpr std::array<uint8_t, 9> ones_u8 {
+            0,
+            1,
+            0b00000011,
+            0b00000111,
+            0b00001111,
+            0b00011111,
+            0b00111111,
+            0b01111111,
+            0b11111111,
+    };
+
+};
+
+typedef BitStreamDeinzer BitStream;
 
 #endif //MEDIENINFO_BITSTREAM_H
